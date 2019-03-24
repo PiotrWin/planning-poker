8b07d8811a77c18a0b4e5295287b1e2b2946e5a4
Piotr Winnicki <p.winnicki1@gmail.com>
2019-03-24 19:42:34 +0100

    Preparing for writing own backend

---
+++ backend/dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _path = _interopRequireDefault(require("path"));
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var dupa = 1;
